c364dfc05da848f08a9223dab23bd884facdfc9b
Alexander Horner <mail@alexanderhorner.com>
2021-12-21 23:15:56 +0100

    Fixed broken img source data and refined option sytem

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const sharp_1 = __importDefault(require("sharp"));
 const schema = {
     type: 'object',
     properties: {
@@ -8,6 +12,20 @@ const schema = {
         },
     },
 };
+const options = {
+    presets: {
+        "thumbnail": [
+            ["resize", 2000, 300],
+            ["flip"],
+            ["resize", 300, 300],
+            ["runPreset", "flip"],
+            ["greyscale"],
+            ["jpeg", { quality: 40 }]
+        ]
+    }
+};
+// Webpack loader config
+module.exports.raw = true; // make sure loader recieves raw input
 async function default_1(source) {
     // const options = this.getOptions();
     // validate(schema, options, {
@@ -15,20 +33,40 @@ async function default_1(source) {
     //     baseDataPath: 'options',
     // });
     var callback = this.async();
-    var buffer = null;
-    // source.toString('hex')
-    // try {
-    //     var sharpInstance = sharp(source.toString('hex'))
-    //     buffer = await sharpInstance.greyscale()
-    //                                 .toBuffer()
-    // } catch (error) {
-    //     var msg = `[Sharp] ${error}`
-    //     console.log(msg)
-    //     var errorError = new Error(msg)
-    //     callback(errorError)
-    //     return
-    // }
-    callback(null, source.toString('utf8'));
+    var buffer;
+    var sharpInstance = process((0, sharp_1.default)(source), options.presets[0].pipeline, options.presets);
+    try {
+        buffer = await sharpInstance.toBuffer();
+    }
+    catch (error) {
+        var msg = `[Sharp] ${error}`;
+        console.log(msg);
+        var errorError = new Error(msg);
+        callback(errorError);
+        return;
+    }
+    callback(null, buffer);
 }
 exports.default = default_1;
+function process(sharpInstance, methodArray, presets) {
+    methodArray.forEach(method => {
+        var methodName = method[0];
+        var args = method.splice(1);
+        switch (methodName) {
+            case "runPreset":
+                var presetName = args[0];
+                process(sharpInstance, presets[presetName], presets);
+                break;
+            default:
+                if (typeof sharpInstance[methodName] === 'function') {
+                    sharpInstance = sharpInstance[methodName](...args);
+                }
+                else {
+                    console.log("Sharp Method", methodName, "doesn't exist.");
+                }
+                ;
+        }
+    });
+    return (sharpInstance);
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { urlToRequest } from 'loader-utils';
 import { validate } from 'schema-utils';
 import { Schema } from 'schema-utils/declarations/ValidationError';
-import { LoaderContext, LoaderDefinition, LoaderDefinitionFunction, LoaderModule, LoaderTargetPlugin, SourceMapDevToolPlugin } from 'webpack';
+import { LoaderContext, LoaderDefinition, LoaderDefinitionFunction, LoaderModule, LoaderTargetPlugin, prefetch, SourceMapDevToolPlugin } from 'webpack';
 
-import sharp from 'sharp'
+import sharp, { Sharp } from 'sharp'
 
 const schema:Schema = {
     type: 'object',
@@ -13,6 +13,29 @@ const schema:Schema = {
         },
     },
 };
+
+
+type Pipeline = Array<Array<any>>
+
+interface Options {
+    presets: Object
+}
+
+const options:Options = {
+    presets: {
+        "thumbnail": [
+            ["resize", 2000, 300],
+            ["flip"],
+            ["resize", 300, 300],
+            ["runPreset", "flip"],
+            ["greyscale"],
+            ["jpeg", { quality: 40}]
+        ]
+    }
+}
+
+// Webpack loader config
+module.exports.raw = true; // make sure loader recieves raw input
 
 export default async function (this:LoaderContext<any>, source: Buffer) {
     // const options = this.getOptions();
@@ -24,23 +47,53 @@ export default async function (this:LoaderContext<any>, source: Buffer) {
 
     var callback = this.async();
 
-    var buffer:Buffer | null = null
-    
-    // source.toString('hex')
+    var buffer:Buffer
 
-    // try {
-    //     var sharpInstance = sharp(source.toString('hex'))
-    //     buffer = await sharpInstance.greyscale()
-    //                                 .toBuffer()
-    // } catch (error) {
-    //     var msg = `[Sharp] ${error}`
+    var sharpInstance = process(sharp(source), options.presets[0].pipeline, options.presets)
 
-    //     console.log(msg)
+
+
+    try {
+        buffer = await sharpInstance.toBuffer()
+    } catch (error) {
+        var msg = `[Sharp] ${error}`
+
+        console.log(msg)
         
-    //     var errorError = new Error(msg)
-    //     callback(errorError)
-    //     return
-    // }
+        var errorError = new Error(msg)
+        callback(errorError)
+        return
+    }
      
-    callback(null, source.toString('utf8'))
+    callback(null, buffer)
+}
+
+function process(sharpInstance:Sharp, methodArray: Pipeline, presets: Object):Sharp {
+
+    methodArray.forEach(method => {
+
+        var methodName:string = method[0]
+        var args = method.splice(1)
+
+        switch (methodName) {
+            case "runPreset":
+                var presetName: string = args[0]
+                process(sharpInstance, presets[presetName], presets)
+                break;
+        
+            default:
+                if ( typeof sharpInstance[methodName] === 'function') {
+                    sharpInstance = sharpInstance[methodName](...args)
+                } else {
+                    console.log("Sharp Method", methodName, "doesn't exist.")
+                };
+        }
+        
+        
+
+    })
+    
+    
+    return (sharpInstance)
+
 }
